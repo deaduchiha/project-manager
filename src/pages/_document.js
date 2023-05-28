9ae08d3eaaeb0059e973d6dd59d66820e34568ba
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-05-28 12:30:31 +0330

    remove next-google-fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { GoogleFonts } from "next-google-fonts";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Tilt+Neon&display=swap" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link
