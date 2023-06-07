0eda943afc93bd712486c43085b9d2a11a3ee54b
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-06-07 17:42:03 +0330

    complete todos api

---
+++ src/pages/api/todos.js
@@ -0,0 +1,46 @@
+import User from "@/models/User";
+import connectDB from "@/utils/connectDB";
+import { getSession } from "next-auth/react";
+
+const handler = async (req, res) => {
+  try {
+    await connectDB();
+  } catch (error) {
+    throw new Error("error in connection to DB");
+  }
+
+  if (!email || !password) {
+    throw new Error("Invalid data");
+  }
+
+  const session = await getSession({ req });
+  if (!session) {
+    return res
+      .status(401)
+      .json({ status: "failed", message: "you are not logged in" });
+  }
+
+  const user = await User.findOne({ email: session.user.email });
+  if (!user) {
+    return res
+      .status(404)
+      .json({ status: "failed", message: "user doesn't exist" });
+  }
+
+  if (req.method === "POST") {
+    const { title, status } = req.body;
+
+    if (!title || !status) {
+      return res
+        .status(422)
+        .json({ status: "failed", message: "invalid data " });
+    }
+
+    user.todos.push({ title, status });
+    user.save;
+
+    res.status(201).json({ status: "success", message: "Todo created" });
+  }
+};
+
+export default handler;
